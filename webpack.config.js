e06b81671c012fd7176e99d37883a8cac3e15075
Ulises Cappa <ulisescappa@gmail.com>
2023-08-07 19:16:32 -0300

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        title: 'APP',
+        filename: 'index.html',
+        template: './src/template.html',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+};
