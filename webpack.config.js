c5c99366442e6af0c760ec948a560578b11db0b7
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-08 11:22:22 -0400

    Fix JS linters

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        title: 'APP',
-        filename: 'index.html',
-        template: './src/template.html',
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    devServer: {
-        static: './dist',
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'APP',
+      filename: 'index.html',
+      template: './src/template.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
 };
