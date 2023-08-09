5b9599e31e5f0e13bd9524dd9ae76cd1ffc527bb
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-09 09:51:39 -0400

    Add like button

--- src/index.js
+++ src/index.js
@@ -11,16 +11,27 @@ async function logTV(item) {
   console.log(movies);
   const card = document.createElement('section');
   const image = document.createElement('img');
+  const midDiv = document.createElement('div');
+  const likeButton = document.createElement('div');
   const title = document.createElement('p');
   const buttonComments = document.createElement('button');
   const buttonReservations = document.createElement('button');
   buttonComments.innerHTML = 'Comments';
   buttonReservations.innerHTML = 'Reservations';
   image.src = `${movies.image.medium}`;
+  image.alt = `${movies.name}`;
   // image.alt
   title.innerHTML = `${movies.name}`;
+  likeButton.innerHTML = '<i class="fa-regular fa-heart"></i>';
+  // likeButton.src = 'assets/heart-regular.svg';
+  // const likeButton = new Image();
+  // likeButton.src = Heartregular;
+  midDiv.appendChild(title);
+  midDiv.appendChild(likeButton);
+  midDiv.className = 'flexdiv';
   card.appendChild(image);
-  card.appendChild(title);
+  card.appendChild(midDiv);
+  card.appendChild(likeButton);
   card.appendChild(buttonComments);
   card.appendChild(buttonReservations);
   tvContainer.appendChild(card);
