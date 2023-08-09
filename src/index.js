f6dfe329ea282d7480ae6c8ab6d1b235eb94a503
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-09 11:54:57 -0400

    Fix like button layout

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ tvArray.forEach(logTV);
 async function logTV(item) {
   const response = await fetch(`https://api.tvmaze.com/shows/${item}`);
   const movies = await response.json();
-  console.log(movies);
   const card = document.createElement('section');
   const image = document.createElement('img');
-  const midDiv = document.createElement('div');
-  const likeButton = document.createElement('div');
+  const midDiv = document.createElement('section');
+  const likeButton = document.createElement('section');
   const title = document.createElement('p');
   const buttonComments = document.createElement('button');
   const buttonReservations = document.createElement('button');
@@ -20,18 +19,17 @@ async function logTV(item) {
   buttonReservations.innerHTML = 'Reservations';
   image.src = `${movies.image.medium}`;
   image.alt = `${movies.name}`;
-  // image.alt
   title.innerHTML = `${movies.name}`;
   likeButton.innerHTML = '<i class="fa-regular fa-heart"></i>';
-  // likeButton.src = 'assets/heart-regular.svg';
-  // const likeButton = new Image();
-  // likeButton.src = Heartregular;
-  midDiv.appendChild(title);
-  midDiv.appendChild(likeButton);
-  midDiv.className = 'flexdiv';
+  midDiv.classList.add('flexdiv');
   card.appendChild(image);
   card.appendChild(midDiv);
-  card.appendChild(likeButton);
+
+  midDiv.innerHTML = title.innerHTML + likeButton.innerHTML;
+
+  // midDiv.appendChild(title);
+  // midDiv.appendChild(likeButton);
+
   card.appendChild(buttonComments);
   card.appendChild(buttonReservations);
   tvContainer.appendChild(card);
