63940321731e1cd97143f5b9a3026599299a85fb
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-08 20:04:08 -0400

    Add grid layout

--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ async function logTV(item) {
   const response = await fetch(`https://api.tvmaze.com/shows/${item}`);
   const movies = await response.json();
   console.log(movies);
+  const card = document.createElement('section');
   const image = document.createElement('img');
   const title = document.createElement('p');
+  const buttonComments = document.createElement('button');
+  const buttonReservations = document.createElement('button');
+  buttonComments.innerHTML = 'Comments';
+  buttonReservations.innerHTML = 'Reservations';
   image.src = `${movies.image.medium}`;
+  // image.alt
   title.innerHTML = `${movies.name}`;
-  tvContainer.appendChild(image);
-  tvContainer.appendChild(title);
+  card.appendChild(image);
+  card.appendChild(title);
+  card.appendChild(buttonComments);
+  card.appendChild(buttonReservations);
+  tvContainer.appendChild(card);
 }
