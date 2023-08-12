41ef7b0122d6d14a6af04ebc9eebe47921388767
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-12 10:18:50 -0400

    Create tests for counter function

--- src/scripts/countMovies.test.js
+++ src/scripts/countMovies.test.js
@@ -1,13 +1,29 @@
 import countMovies from './countMovies.js';
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>');
+global.document = dom.window.document;
 
-let movieArray = [1, 3, 5, 7];
+const movie1 = document.createElement('div');
+const movie2 = document.createElement('div');
+const movie3 = document.createElement('div');
+
+movie1.classList.add('howManyMovies');
+movie2.classList.add('howManyMovies');
+movie3.classList.add('howManyMovies');
+
+document.body.appendChild(movie1);
+document.body.appendChild(movie2);
+document.body.appendChild(movie3);
 
 describe('Test counter function', () => {
   test('Count movies', () => {
-    expect(countMovies(movieArray)).toEqual(4);
+    expect(countMovies('.howManyMovies')).toBe(3);
   });
-  test('Remove element from movies, then count', () => {
-    movieArray.pop();
-    expect(countMovies(movieArray)).toEqual(3);
+  test('Add a movie and count again', () => {
+    const movie4 = document.createElement('div');
+    movie4.classList.add('howManyMovies');
+    document.body.appendChild(movie4);
+
+    expect(countMovies('.howManyMovies')).toBe(4);
   });
 });
